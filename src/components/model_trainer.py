98c48761e2b320531c7602903058199447d69e6c
reshma <reshma76kh@gmail.com>
2026-07-18 15:51:16 +0530

    Model Trainer Done

---
+++ src/components/model_trainer.py
@@ -0,0 +1,100 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+from src.util import save_object, evaluate_models
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join(
+        "artifacts",
+        "model.pkl"
+    )
+
+
+class ModelTrainer:
+
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array):
+
+        try:
+
+            logging.info("Splitting training and testing input data")
+
+            x_train = train_array[:, :-1]
+            y_train = train_array[:, -1]
+
+            x_test = test_array[:, :-1]
+            y_test = test_array[:, -1]
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+            params = {
+                "Random Forest": {},
+                "Decision Tree": {},
+                "Gradient Boosting": {},
+                "Linear Regression": {},
+                "XGBRegressor": {},
+                "CatBoosting Regressor": {},
+                "AdaBoost Regressor": {},
+            }
+
+            model_report = evaluate_models(
+                X_train=x_train,
+                y_train=y_train,
+                X_test=x_test,
+                y_test=y_test,
+                models=models,
+                param=params
+            )
+
+            best_model_score = max(model_report.values())
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best model found", sys)
+
+            best_model.fit(x_train, y_train)
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(x_test)
+
+            r2_square = r2_score(y_test, predicted)
+
+            return r2_square
+
+        except Exception as e:
+            raise CustomException(e, sys)
